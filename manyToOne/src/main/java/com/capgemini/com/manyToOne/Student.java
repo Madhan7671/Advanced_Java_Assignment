d55ea5fd972bbbe4f7c1d02b0f02817f453d52d3
MadhanMaddasani <118039222+Madhan7671@users.noreply.github.com>
2026-02-20 12:20:18 +0530

    Add files via upload

---
+++ manyToOne/src/main/java/com/capgemini/com/manyToOne/Student.java
@@ -0,0 +1,61 @@
+package com.capgemini.com.manyToOne;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="student")
+public class Student {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name="name")
+	private String name;
+	
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name="departmentId")
+	private Department department;
+
+	public Student() {
+		
+	}
+	public Student(Long id, String name, Department department) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.department = department;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Department getDepartment() {
+		return department;
+	}
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", department=" + department + "]";
+	}
+	
+	
+}
